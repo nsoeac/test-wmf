5f73cc799156408c67c9d913b826daa2977d10b0
nsoeac <291334262+nsoeac@users.noreply.github.com>
2026-08-15 20:38:27 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+int main() {
+    std::println("Hello, World!");
+}
---
+++ src/pch.hpp
@@ -0,0 +1,45 @@
+#pragma warning(push)
+#pragma warning(disable : 4201)
+
+// Windows.
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define UNICODE
+#include <windows.h>
+
+// Windows Sockets 2.
+
+#include <winsock2.h>
+#include <iphlpapi.h>
+
+// Windows Media Foundation.
+
+#include <codecapi.h>
+#include <d3d11.h>
+#include <d3d9.h>
+#include <d3d9caps.h>
+#include <d3d9types.h>
+#include <dxva.h>
+#include <dxva2api.h>
+#include <dxvahd.h>
+#include <evr.h>
+#include <evr9.h>
+#include <mfapi.h>
+#include <mfcaptureengine.h>
+#include <mferror.h>
+#include <mfidl.h>
+#include <mfmediacapture.h>
+#include <mfmediaengine.h>
+#include <mfmp2dlna.h>
+#include <mfobjects.h>
+#include <mfplay.h>
+#include <mfreadwrite.h>
+#include <mftransform.h>
+#include <opmapi.h>
+#include <wmcodecdsp.h>
+#include <wmcontainer.h>
+
+#pragma warning(pop)
+
+#include <print>
