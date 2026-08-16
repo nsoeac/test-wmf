5e7c3f03d698b88a9a7d367225f0d960dfce9c49
nsoeac <291334262+nsoeac@users.noreply.github.com>
2026-08-16 20:57:48 +0100

    Move networking into struct

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@ static constexpr int packet_size = 1'384;
 static constexpr int header_size = sizeof(Packet_Header);
 
 static Config read_config() {
+    std::println("Reading config");
+
     std::ifstream input_stream(config_path.data(), std::ios::in | std::ios::binary | std::ios::ate);
 
     if (!input_stream) {
@@ -100,10 +102,122 @@ static int64_t get_sample_time(int64_t start_timestamp, int64_t end_timestamp) {
     return sample_time;
 }
 
+struct App {
+    SOCKET socket = INVALID_SOCKET;
+
+    int video_width = -1;
+    int video_height = -1;
+    int video_framerate = -1;
+
+    std::vector<uint8_t> receive_buffer = std::vector<uint8_t>(packet_size);
+    WSABUF wsa_receive_buffer = {};
+    DWORD bytes_received = 0;
+
+    App() {
+        wsa_receive_buffer.buf = (CHAR *)receive_buffer.data();
+        wsa_receive_buffer.len = (ULONG)receive_buffer.size();
+    }
+
+    void connect(Config &config) {
+        ADDRINFOW hints = {};
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_DGRAM;
+        hints.ai_protocol = IPPROTO_UDP;
+        PADDRINFOW send_info = NULL;
+        if (GetAddrInfoW(config.address.data(), config.port.data(), &hints, &send_info) != 0) {
+            THROW_WSA(GetAddrInfoW);
+        }
+
+        socket = WSASocketW(hints.ai_family, hints.ai_socktype, hints.ai_protocol, NULL, 0, 0);
+        if (socket == INVALID_SOCKET) {
+            THROW_WSA(WSASocketW);
+        }
+
+        if (WSAConnect(socket, send_info->ai_addr, sizeof(sockaddr_in), NULL, NULL, NULL, NULL) == SOCKET_ERROR) {
+            THROW_WSA(WSAConnect);
+        }
+
+        std::vector<uint8_t> send_buffer(packet_size);
+        WSABUF wsa_send_buffer = {};
+        wsa_send_buffer.buf = (CHAR *)send_buffer.data();
+        wsa_send_buffer.len = packet_size;
+        DWORD bytes_sent = 0;
+        if (WSASend(socket, &wsa_send_buffer, 1, &bytes_sent, 0, NULL, NULL) != 0) {
+            THROW_WSA(WSASend);
+        }
+
+        sockaddr server_address = {};
+        INT server_address_length = sizeof(server_address);
+        DWORD receive_flags = 0;
+        if (WSARecvFrom(socket, &wsa_receive_buffer, 1, &bytes_received, &receive_flags, &server_address, &server_address_length, NULL, NULL) != 0) {
+            THROW_WSA(WSARecvFrom);
+        }
+
+        if (bytes_received < 4) {
+            throw std::runtime_error("Expected 4 bytes for the server receive port");
+        }
+
+        std::span<int32_t> initial_values = { (int32_t *)(receive_buffer.data()), (int32_t *)(receive_buffer.data() + sizeof(int32_t) * 4) };
+
+        uint16_t server_port = (uint16_t)initial_values[0];
+        if (server_port == 0) {
+            throw std::runtime_error(std::format("Server receive port is {}", server_port));
+        }
+
+        video_width = initial_values[1];
+        video_height = initial_values[2];
+        video_framerate = initial_values[3];
+
+        ((sockaddr_in *)&server_address)->sin_port = htons(server_port);
+        std::println("Received {} bytes; server port is {}", bytes_received, server_port);
+
+        if (WSAConnect(socket, &server_address, sizeof(sockaddr_in), NULL, NULL, NULL, NULL) == SOCKET_ERROR) {
+            THROW_WSA(WSAConnect);
+        }
+
+        std::println("Connected to server", bytes_received, server_port);
+
+        if (WSASend(socket, &wsa_send_buffer, 1, &bytes_sent, 0, NULL, NULL) != 0) {
+            THROW_WSA(WSASend);
+        }
+
+        std::println("Sent first packet to server", bytes_received, server_port);
+    }
+
+    [[nodiscard]] std::vector<uint8_t> receive() {
+        std::vector<uint8_t> result(packet_size);
+
+        {
+            DWORD flags = 0;
+            if (WSARecv(socket, &wsa_receive_buffer, 1, &bytes_received, &flags, NULL, NULL) != 0) {
+                THROW_WSA(WSARecv);
+            }
+        }
+
+        std::memcpy(result.data(), receive_buffer.data(), bytes_received);
+        result.resize(bytes_received);
+
+        return result;
+    }
+};
+
 int main() {
     std::println("Initialising COM");
 
     hresult(CoInitialize(NULL));
+
+    std::println("Initialising WSA");
+
+    {
+        WSADATA wsa_data = {};
+        if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+            THROW_WSA(WSAStartup);
+        }
+    }
+
+    Config config = read_config();
+
+    // Initialising app networking.
 
     std::println("Creating decoder");
 
@@ -135,68 +249,8 @@ int main() {
         CoTaskMemFree(activates);
     }
 
-    std::println("Starting WSA");
-
-    WSADATA wsa_data = {};
-    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-        THROW_WSA(WSAStartup);
-    }
-
-    Config config = read_config();
-
-    ADDRINFOW hints = {};
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = IPPROTO_UDP;
-    PADDRINFOW send_info = NULL;
-    if (GetAddrInfoW(config.address.data(), config.port.data(), &hints, &send_info) != 0) {
-        THROW_WSA(GetAddrInfoW);
-    }
-
-    SOCKET socket = WSASocketW(hints.ai_family, hints.ai_socktype, hints.ai_protocol, NULL, 0, 0);
-    if (socket == INVALID_SOCKET) {
-        THROW_WSA(WSASocketW);
-    }
-
-    if (WSAConnect(socket, send_info->ai_addr, sizeof(sockaddr_in), NULL, NULL, NULL, NULL) == SOCKET_ERROR) {
-        THROW_WSA(WSAConnect);
-    }
-
-    std::vector<uint8_t> send_buffer(packet_size);
-    WSABUF wsa_send_buffer = {};
-    wsa_send_buffer.buf = (CHAR *)send_buffer.data();
-    wsa_send_buffer.len = packet_size;
-    DWORD bytes_sent = 0;
-    if (WSASend(socket, &wsa_send_buffer, 1, &bytes_sent, 0, NULL, NULL) != 0) {
-        THROW_WSA(WSASend);
-    }
-
-    std::vector<uint8_t> receive_buffer(packet_size);
-    WSABUF wsa_receive_buffer = {};
-    wsa_receive_buffer.buf = (CHAR *)receive_buffer.data();
-    wsa_receive_buffer.len = packet_size;
-    DWORD bytes_received = 0;
-    sockaddr server_address = {};
-    INT server_address_length = sizeof(server_address);
-    DWORD receive_flags = 0;
-    if (WSARecvFrom(socket, &wsa_receive_buffer, 1, &bytes_received, &receive_flags, &server_address, &server_address_length, NULL, NULL) != 0) {
-        THROW_WSA(WSARecvFrom);
-    }
-
-    if (bytes_received < 4) {
-        throw std::runtime_error("Expected 4 bytes for the server receive port");
-    }
-
-    std::span<int32_t> initial_values = { (int32_t *)(receive_buffer.data()), (int32_t *)(receive_buffer.data() + sizeof(int32_t) * 4) };
-
-    uint16_t server_port = (uint16_t)initial_values[0];
-    if (server_port == 0) {
-        throw std::runtime_error(std::format("Server receive port is {}", server_port));
-    }
-
-    int video_width = initial_values[1];
-    int video_height = initial_values[2];
-    int video_framerate = initial_values[3];
+    App app;
+    app.connect(config);
 
     // Set input and output types.
 
@@ -211,7 +265,7 @@ int main() {
         GUID subtype = {};
         hresult(input->GetGUID(MF_MT_SUBTYPE, &subtype));
         if ((major_type == MFMediaType_Video) && (subtype == MFVideoFormat_HEVC)) {
-            hresult(MFSetAttributeSize(input.Get(), MF_MT_FRAME_SIZE, video_width, video_height));
+            hresult(MFSetAttributeSize(input.Get(), MF_MT_FRAME_SIZE, app.video_width, app.video_height));
             hresult(decoder->SetInputType(0, input.Get(), 0));
             std::println("Input type set");
             break;
@@ -256,41 +310,22 @@ int main() {
     output_sample_buffer.dwStreamID = 0;
     output_sample_buffer.pSample = output_sample.Get();
 
-    ((sockaddr_in *)&server_address)->sin_port = htons(server_port);
-    std::println("Received {} bytes; server port is {}", bytes_received, server_port);
-
-    if (WSAConnect(socket, &server_address, sizeof(sockaddr_in), NULL, NULL, NULL, NULL) == SOCKET_ERROR) {
-        THROW_WSA(WSAConnect);
-    }
-
-    std::println("Connected to server", bytes_received, server_port);
-
-    if (WSASend(socket, &wsa_send_buffer, 1, &bytes_sent, 0, NULL, NULL) != 0) {
-        THROW_WSA(WSASend);
-    }
-
-    std::println("Sent first packet to server", bytes_received, server_port);
-
     ComPtr<IMFSample> parameter_sets_sample;
     int64_t start_timestamp = INT64_MIN;
-    int64_t sample_duration = 10'000'000 /* Hundred-nanoseconds in a second. */ / video_framerate;
+    int64_t sample_duration = 10'000'000 /* Hundred-nanoseconds in a second. */ / app.video_framerate;
     std::vector<ComPtr<IMFSample>> video_samples;
     while (true) {
-        DWORD flags = 0;
-        if (WSARecv(socket, &wsa_receive_buffer, 1, &bytes_received, &flags, NULL, NULL) != 0) {
-            THROW_WSA(WSARecv);
-        }
+        std::vector<uint8_t> packet = app.receive();
 
-        Packet_Header &header = *(Packet_Header *)receive_buffer.data();
+        Packet_Header &header = *(Packet_Header *)packet.data();
         assert(header.format_index >= 0);
 
-        std::span<uint8_t> payload = { receive_buffer.begin() + header_size, receive_buffer.begin() + bytes_received };
+        std::span<uint8_t> payload = { packet.begin() + header_size, packet.end() };
         bool has_parameter_sets = is_parameter_sets(payload);
 
-        std::println("Received {} bytes: {{ frame_index: {}, packet_index: {}, packet_count: {}, format_index: {}: timestamp: {} }}", bytes_received, header.frame_index, header.packet_index, header.packet_count, header.format_index, header.timestamp);
+        std::println("Received {} bytes: {{ frame_index: {}, packet_index: {}, packet_count: {}, format_index: {}: timestamp: {} }}", app.bytes_received, header.frame_index, header.packet_index, header.packet_count, header.format_index, header.timestamp);
 
         if (has_parameter_sets) {
-
             assert(parameter_sets_sample == nullptr);
 
             if (start_timestamp == INT64_MIN) {
